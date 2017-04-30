162c92c6eff171aa3b29ffd9fde0db89887919d9
Cameron Zurmuhl <cameron.zurmuhl@verizon.net>
2017-04-30 16:31:34 -0400

    Changed from Interface to Abstract Class

--- Facility.java
+++ Facility.java
@@ -1,32 +1,56 @@
-
 /**
  * The interface facility can represent any building
  * For this project this will mean a warehouse or a shop
  * 
  * @author Emmett O'Toole 
- * @version (4-23-17
+ * @version (4-23-17)
+ * Revised by Cameron Zurmuhl 4/30/2017 4:27
  */
-public interface Facility
+public abstract class Facility implements Comparable <Facility>
 {
+    private int ID;
+    private Character letter;
     /**
      * Distance from method
      * 
      * @param  Facility any    A facility from which to calculate the distance
      * @return  int   The distance between the two facilities
      */
-    int distanceFrom(Facility any);
-    
+    public abstract int distanceFrom(Facility any);
+
     /**
      * Get id method
      * Every facility must be able to get a unique id for that facility
      * @return int    The id of the facility 
      */
-    int getID();
-    
+    public abstract int getID();
+
     /**
      * Get location method
      * Every facility must be able to get it's location
      * @return Location     The location of the facility
      */
-    Location getLocation();
+    public abstract Location getLocation();
+
+    @Override
+    public int compareTo(Facility l)
+    {
+        if(this.letter.compareTo(l.letter) == 0){
+            if(this.ID < l.ID){
+                return -1;
+            } else if (this.ID > l.ID){
+                return 1;
+            } else {
+                return 0;
+            }
+        } else{
+            if(this.ID < l.ID){
+                return -1;
+            } else if (this.ID > l.ID){
+                return 1;
+            } else {
+                return this.letter.compareTo(l.letter);
+            }
+        }
+    }
 }
