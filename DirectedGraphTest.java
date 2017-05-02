e6fd9ed7b15684157ee6b3303b54a3825b9d4f15
otoolee14 <otoolee@lafayette.edu>
2017-05-02 14:50:32 -0400

    Add files via upload

--- DirectedGraphTest.java
+++ DirectedGraphTest.java
@@ -1,3 +1,4 @@
+
 import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
@@ -8,7 +9,7 @@ import org.junit.Test;
  *
  * @author  Emmett O'Toole
  * @version 4-30-17
- * Revised by Cameron Zurmuhl 4/20/2017 10:33 p.m
+ * Additions Emmett O'Toole 2:48 P.M 5-2-17
  */
 public class DirectedGraphTest
 {
@@ -19,7 +20,6 @@ public class DirectedGraphTest
     @Test
     public void graphTest(){
         DirectedGraph test= new DirectedGraph();
-        //add four shops
         Shop one=new Shop(1,new Location(10,10));
         Shop two =new Shop(2,new Location(20,20));
         Shop three=new Shop(3,new Location(30,30));
@@ -28,52 +28,44 @@ public class DirectedGraphTest
         assertEquals(test.addNode(two),true);
         assertEquals(test.addNode(three),true);
         assertEquals(test.addNode(four),true);
-        
-        //make sure one shop cannot be added twice
-        assertEquals(test.addNode(four), false);
         assertEquals(test.addNode(new Shop(1,new Location(10,10))),false);
-        
-        //add edges between nodes 
         assertEquals(test.addEdge(one,two,5),true);
         assertEquals(test.addEdge(one,three,2),true);
         assertEquals(test.addEdge(two,three,6),true);
         assertEquals(test.addEdge(four,two,8),true);
-   
-        
-        //test that we can aquire a facility 
-        assertEquals(test.getFacility(new Shop(12,new Location(50,50))),null); //should not return anything since this facility is not in the graph
-        assertEquals(test.getFacility(one), one); //return facility one
-        
-        //test return closest neighbor for all nodes 
+        assertEquals(test.getFacility(one),one);
+        assertEquals(test.getFacility(new Shop(12,new Location(50,50))),null);
         assertEquals(test.returnClosestNeighbor(one),three);
         assertEquals(test.returnClosestNeighbor(two),one);
-        assertEquals(test.returnClosestNeighbor(three), one);
-        assertEquals(test.returnClosestNeighbor(four), two);
-        
-        //check each's node's neighborhood set size
-        assertEquals(test.getNeighbors(one).size(), 2 );
-        assertEquals(test.getNeighbors(two).size(), 3 );
-        assertEquals(test.getNeighbors(three).size(), 2 );
-        assertEquals(test.getNeighbors(four).size(), 1 );
-        
-        //Check that the first vertex in the neighborhood list is the closest neighbor (tests if sorting is working)
-        assertEquals(test.getNeighbors(one).get(0), three);
-        assertEquals(test.getNeighbors(two).get(0), one);
-        assertEquals(test.getNeighbors(three).get(0), one);
-        assertEquals(test.getNeighbors(four).get(0), two);
-        
-        //check that we can obtain each edge's weight
-        assertEquals(test.getEdgeWeight(one, two), 5);
-        assertEquals(test.getEdgeWeight(two, one), 5);
-        assertEquals(test.getEdgeWeight(one, three), 2);
-        assertEquals(test.getEdgeWeight(three, one), 2);
-        assertEquals(test.getEdgeWeight(two, three), 6);
-        assertEquals(test.getEdgeWeight(three, two), 6);
-        assertEquals(test.getEdgeWeight(four, two), 8);
-        assertEquals(test.getEdgeWeight(two, four), 8);
-        assertEquals(test.getEdgeWeight(four, one), -1); //edge does not exist
-        
-        //do a breadthFirstClosest search
-        test.breadthFirstClosest(one);
+    }
+
+    /**
+     * Tests create edges method
+     * 
+     */
+    @Test
+    public void edgesTest(){
+        DirectedGraph test= new DirectedGraph();
+        Shop one=new Shop(1,new Location(10,10));
+        Shop two =new Shop(2,new Location(20,20));
+        Shop three=new Shop(3,new Location(30,30));
+        Shop four=new Shop(4,new Location(40,40));
+        assertEquals(test.addNode(one),true);
+        assertEquals(test.addNode(two),true);
+        assertEquals(test.addNode(three),true);
+        assertEquals(test.addNode(four),true);
+        test.createEdges();
+        assertEquals(test.getEdgeWeight(one,two),20);
+        assertEquals(test.getEdgeWeight(one,three),40);
+        assertEquals(test.getEdgeWeight(one,four),60);
+        assertEquals(test.getEdgeWeight(two,one),20);
+        assertEquals(test.getEdgeWeight(two,three),20);
+        assertEquals(test.getEdgeWeight(two,four),40);
+        assertEquals(test.getEdgeWeight(three,one),40);
+        assertEquals(test.getEdgeWeight(three,two),20);
+        assertEquals(test.getEdgeWeight(three,four),20);
+        assertEquals(test.getEdgeWeight(four,one),60);
+        assertEquals(test.getEdgeWeight(four,two),40);
+        assertEquals(test.getEdgeWeight(four,three),20);
     }
 }
